a6daa881e47182430900966b03dbc2069d51e7de
dnrgroupbv-oss <dnrgroup.bv@gmail.com>
2026-04-02 19:28:30 +0200

    app.py

--- app.py
+++ app.py
@@ -90,4 +90,7 @@ def add():
     save(data)
     return redirect("/panel")
 
-app.run()
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
